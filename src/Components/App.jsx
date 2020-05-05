853931c54d60595a4133518bfb1abbc2d27b9031
raj-pat <rajp4445@gmail.com>
2020-05-05 16:49:47 -0700

    Resolve redux config error

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import Table from "./Table";
-import {Provider} from "react-redux"
-import store  from "../Store/Store";
+import { Provider } from "react-redux";
+import store from "../Store/Store";
 
 class App extends Component {
   state = {};
 
   render() {
     return (
-        
-      
+      <Provider store={store}>
         <Table />
-     
+      </Provider>
+      // <Table />
     );
   }
 }
--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,17 +1,42 @@
 import { getMovies } from "../Starter Code/services/fakeMovieService";
 import { createStore } from "redux";
 
-export function reducer(state = getMovies(), action) {
-  //   console.log(getMovies());
+// export function reducer(state = getMovies(), action) {
+//   //   console.log(getMovies());
+//   switch (action.type) {
+//     case "DELETE":
+//       return state;
+//     case "del2":
+//       return state;
+//     default:
+//       return state;
+//   }
+// }
+// let store = createStore(
+//   reducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+// export default store;
+// console.log(store.getState());
+
+function todos(state = [], action) {
   switch (action.type) {
-    case "DELETE":
-      return state;
-    case "del2":
-      return state;
+    case "ADD_TODO":
+      return state.concat([action.text]);
     default:
       return state;
   }
 }
-let store = createStore(reducer);
-export default store
-// console.log(store.getState());
+let store = createStore(
+  todos,
+  ["Use Redux"],
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.dispatch({
+  type: "ADD_TODO",
+  text: "Read the docs",
+});
+
+console.log(store.getState());
+export default store;
